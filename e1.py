fbdb71ef3c1a7847e55e77ceb5747c1f82370db7
Maria Romero <m.romeroangulo@gmail.com>
2017-03-06 14:32:52 +0100

    Delete e1.py

--- e1.py
+++
@@ -1,7 +0,0 @@
-import json
-with open('resumen-accidentes-2015.json') as data_file:
-	data = json.load(data_file)
-
-#1. Lista la fecha, el tipo de accidente y numero de vehiculos involucrados.	
-for accidentes in data:
-	print accidentes["FECHA"]+" "+accidentes["TIPO DE ACCIDENTE"]+" " +accidentes["NUMERO DE VEHICULOS"]+"."
